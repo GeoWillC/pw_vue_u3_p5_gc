6fd8d2128e53aad0e50bff7d4f4eecaaf0790b1e
GeoWillC <gwconlago@uce.edu.ec>
2024-02-15 11:14:04 -0500

    Taller 34 completado

--- src/components/Estudiante.vue
+++ src/components/Estudiante.vue
@@ -2,9 +2,32 @@
   <h1>Componente Estudiante</h1>
   <input v-model="id" type="text">
   <button @click="consultarPorId">Consultar</button>
-  <input v-model="nombre" type="text">
-  <input v-model="apellido" type="text">
-  <button @click="insertar">Insertar</button>
+  <button @click="consultarPorId">Eliminar</button>
+  <p></p>
+  <div class="form">
+    <h3>Datos del estudiante</h3>
+    <p>Nombre</p>
+    <input v-model="nombre" type="text">
+    <p>Apellido</p>
+    <input v-model="apellido" type="text">
+    <p>Cedula</p>
+    <input v-model="cedula" type="text">
+    <p>Edad</p>
+    <input v-model="edad" type="text">
+    <p>Fecha de nacimiento</p>
+    <input v-model="fechaNacimiento" type="datetime-local" >
+    <p>Genero</p>
+    <input v-model="genero" type="text">
+    <p>Hobby</p>
+    <input v-model="hobby" type="text">
+    <p>Año</p>
+    <input v-model="anio" type="text">
+    <p>Paralelo</p>
+    <input v-model="paralelo" type="text">
+    <p></p>
+    <button @click="insertar">Insertar</button>
+    <p></p>
+  </div>
 </template>
   
 <script>
@@ -16,7 +39,14 @@ export default {
     return {
       id: null,
       nombre: null,
-      apellido: null
+      apellido: null,
+      cedula: null,
+      edad: null,
+      fechaNacimiento: null,
+      genero: null,
+      hobby: null,
+      anio: null,
+      paralelo: null
     }
   },
   methods: {
@@ -25,6 +55,13 @@ export default {
       const data = await consultarEstudianteIdFachada(this.id);
       this.nombre = data.name
       this.apellido = data.apellido
+      this.cedula = data.cedula
+      this.edad = data.edad
+      this.fechaNacimiento = data.fechaNacimiento
+      this.genero = data.genero
+      this.hobby = data.hobby
+      this.anio = data.anio
+      this.paralelo = data.paralelo
       console.log('Desde componente')
       console.log(data)
     },
@@ -33,13 +70,13 @@ export default {
       const estuBody = {
         name: this.nombre,
         apellido: this.apellido,
-        genero: "M",
-        fechaNacimiento: "2002-05-16T00:00:00",
-        cedula: "1232312312",
-        edad: "22",
-        hobby: "futbol",
-        paralelo: "B",
-        anio: "3 BGU"
+        genero: this.genero,
+        fechaNacimiento: this.fechaNacimiento,
+        cedula: this.cedula,
+        edad: this.edad,
+        hobby: this.hobby,
+        paralelo: this.paralelo,
+        anio: this.anio
       }
       await insertarFachada(estuBody);
       console.log(estuBody)
@@ -52,5 +89,25 @@ export default {
 </script>
   
   <!-- Add "scoped" attribute to limit CSS to this component only -->
-<style scoped></style>
+<style scoped>
+ .form{
+  border-style: solid;
+  margin-left: auto;
+    margin-right: auto;
+  border-radius: 8%;
+  width: 30%;
+  padding-left: 10px;
+  text-align: left;
+  padding-left: 50px;
+  background-color: rgb(42, 134, 177);
+
+
+ }
+ p{
+ 
+ }
+ h3{
+  
+ }
+ </style>
   
